d34f9200f3883077446e87d5a001eb341de0d489
WellsonLau <elwellson@gmail.com>
2019-03-02 04:41:25 -0800

    movement system
    complete for all 4 directions.
    added move method that moves in x and y directions when called upon.
    actionPerformed will call move based on the key inputs from the user.

--- HackMerced/src/HealthyChoices/Gameplay.java
+++ HackMerced/src/HealthyChoices/Gameplay.java
@@ -22,6 +22,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
     private int mainy = 300;
     private boolean right;
     private boolean up;
+    private boolean down;
+    private boolean left;
     
 	public Gameplay() {
 		addKeyListener(this);					//Makes the key listener and timer when the game object is created
@@ -46,11 +48,68 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	@Override
 	
 	public void actionPerformed(ActionEvent e) {
-		if(right && up){ //move northeast if D and W is pressed simultaneously
-			mainy += -10;
-			mainx += 10;
-			repaint();
+		if(right){ //if right and possibly some button pressed simultaneously, move this direction.
+			if(up) { //right and up, move northeast.
+				move(10, -10);
+			}
+			else if(down) { //right and down, move southeast
+				move(10, 10);
+			}
+			else if(left) { //right and left, stay in place.
+				move(0, 0);
+			}
+			else { //else, right is the only key pressed, so move right only
+				move(10, 0);
+			}
 		}
+		else if(left) { //if left and possibly some button pressed simultaneously, move this direction.
+			if(up) { //left and up, move northwest
+				move(-10, -10);
+			}
+			else if(down) { //left and down, move southwest
+				move(-10, 10);
+			}
+			else if(right) { //left and right, stay in place.
+				move(0, 0);
+			}
+			else { //move left if that's the only key pressed
+				move(-10, 0);
+			}
+		}
+		else if(up) { //if up and possibly some button pressed simultaneously, move this direction.
+			if(down) { //up and down, stay in place
+				move(0, 0);
+			}
+			else if(right) { //up and right, move northeast
+				move(10, -10);
+			}
+			else if(left) { //up and left, move northwest
+				move(-10, -10);
+			}
+			else { 
+				move(0, -10); //move up if up is the only key pressed
+			}
+		}
+		else if(down) {
+			if(up) { //down and up, stay in place.
+				move(0, 0);
+			}
+			else if(right) { //down and right, move southeast
+				move(10, 10);
+			}
+			else if(left) { //down and left, move southwest
+				move(-10, 10);
+			}
+			else { //only down is pressed, so move down
+				move(0, 10);
+			}
+		}
+	}
+
+	public void move(int xDir, int yDir) {
+		mainx += xDir;
+		mainy += yDir;
+		repaint();
 	}
 	
 	@Override
@@ -61,6 +120,12 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		if(e.getKeyCode() == KeyEvent.VK_W) {
 			up = true;
 		}
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			down = true;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			left = true;
+		}
 	}
 
 	@Override
@@ -70,6 +135,12 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		}
 		if(e.getKeyCode() == KeyEvent.VK_W) {
 			up = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			down = false;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			left = false;
 		}
 	}
 	@Override
