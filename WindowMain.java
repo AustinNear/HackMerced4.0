2e3617f7b505ee7eb6da3190fbe5c26ba8a1a796
AustinNear <46238739+AustinNear@users.noreply.github.com>
2019-03-02 18:11:11 -0800

    Update WindowMain.java

--- WindowMain.java
+++ WindowMain.java
@@ -6,12 +6,22 @@ import javax.swing.JFrame;
 public class WindowMain {
 	public static JFrame Window = new JFrame();
 	public static Gameplay game = new Gameplay();
+	public static Start start = new Start();
 	public static void main (String[] args) {
 		Window.setBounds(0, 0, 905, 700);	
+		Window.add(start);
+		Window.setVisible(true);		
 		Window.setResizable(false);
-		Window.setTitle("Healthy Choices");
+		Window.setTitle("Healthy Bobo");
 		Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Window.add(game);
-		Window.setVisible(true);
+	}
+	public static void changemode(boolean a) {
+		if(a) {
+			Window.setVisible(false);
+			Window.remove(start);
+			Window.add(game);
+			Window.setVisible(true);
+		}
+		
 	}
 }
